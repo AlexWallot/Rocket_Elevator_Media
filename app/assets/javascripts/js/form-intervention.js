c5536a86c02e24188c60ecfe00e8c9afeeea26b2
AlexWallot <alexwallot@outlook.com>
2021-11-30 09:03:40 -0500

    js and css of form work

---
+++ app/assets/javascripts/js/form-intervention.js
@@ -0,0 +1,103 @@
+$(document).ready(function () {
+    $('#buildingDropdown').hide();
+    $('#batteryDropdown').hide();
+    $('#columnDropdown').hide();
+    $('#elevatorDropdown').hide();
+});
+
+function buildingDropdownList() {
+    if ($('#customer').val() == "") {
+        $('#buildingDropdown').hide();
+        $('#batteryDropdown').hide();
+        $('#columnDropdown').hide();
+        $('#elevatorDropdown').hide();
+    }else{
+        $('#buildingDropdown').hide();
+        $('#batteryDropdown').hide();
+        $('#columnDropdown').hide();
+        $('#elevatorDropdown').hide();
+        $.ajax({
+            url: "/intervention/building",
+            type: "GET",
+            data: {customer: $('#customer').val()},
+            success: function(data) {
+                var list = [];
+                $.each(data,function(key, value) { 
+                    list += '<option value="' + value.id + '">' + value.fullNameAdministrator + '</option>';    
+                });
+                $('#building').empty();
+                $('#building').append('<option value="">Select Building</option>');
+                $('#building').append(list);
+            }
+        })
+        $('#buildingDropdown').show();
+    }
+};
+
+function batteryDropdownList() {
+    if ($('#building').val() == "") {
+        $('#batteryDropdown').hide();
+        $('#columnDropdown').hide();
+        $('#elevatorDropdown').hide();
+    }else{
+        $.ajax({
+            url: "/intervention/battery",
+            type: "GET",
+            data: {building: $('#building').val()},
+            success: function(data) {
+                console.log(data);
+                var list = [];
+                $.each(data,function(key, value) { 
+                    list += '<option value="' + value.id + '">' + value.types + '</option>';    
+                });
+                $('#battery').empty();
+                $('#battery').append('<option value="">Select Battery</option>');
+                $('#battery').append(list);
+            }
+        })
+        $('#batteryDropdown').show();
+    }
+};
+
+function columnDropdownList() {
+    if ($('#battery').val() == "") {
+        $('#columnDropdown').hide();
+        $('#elevatorDropdown').hide();
+    }else{
+        $.ajax({
+            url: "/intervention/column",
+            type: "GET",
+            data: {battery: $('#battery').val()},
+            success: function(data) {
+                console.log(data);
+                var list = [];
+                $.each(data,function(key, value) { 
+                    list += '<option value="' + value.id + '">' + value.types + '</option>';    
+                });
+                $('#column').empty();
+                $('#column').append('<option value="">None</option>');
+                $('#column').append(list);
+            }
+        })
+        $('#columnDropdown').show();
+        $('#elevatorDropdown').show();
+    }
+};
+
+function elevatorDropdownList() {
+    $.ajax({
+        url: "/intervention/elevator",
+        type: "GET",
+        data: {column: $('#column').val()},
+        success: function(data) {
+            console.log(data);
+            var list = [];
+            $.each(data,function(key, value) { 
+                list += '<option value="' + value.id + '">' + value.serialNumber + '</option>';    
+            });
+            $('#elevator').empty();
+            $('#elevator').append('<option value="">None</option>');
+            $('#elevator').append(list);
+        }
+    })
+};
