793a2ecbea9cdac95a6889b361773462e7d5d3f3
AlexWallot <alexwallot@outlook.com>
2021-12-01 11:15:48 -0500

    app2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,6 @@ require 'sendgrid-ruby'
 class ApplicationController < ActionController::Base
     include SendGrid
     helper_method :watson
-    skip_before_filter :verify_authenticity_token
     protect_from_forgery prepend: true
     
     def watson
