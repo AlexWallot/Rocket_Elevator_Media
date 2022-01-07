02620fb0f824c9449ecf14d7bea9e54a50ac1a44
AlexWallot <alexwallot@outlook.com>
2022-01-07 13:35:33 -0500

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,17 @@ Rails.application.routes.draw do
   get 'pages/charts'
   get 'pages/admin_stats'
   get 'pages/intervention'
+  get 'pages/machine_learning'
   get '/quote/new', to: 'quote#new'
   get '/intervention/building', to: 'intervention#building'
   get '/intervention/battery', to: 'intervention#battery'
   get '/intervention/column', to: 'intervention#column'
   get '/intervention/elevator', to: 'intervention#elevator'
+  get '/identification/selectedProfile', to: 'identification#selectedProfile'
   post '/quote', to: 'quote#create'
   post '/intervention', to: 'intervention#create'
   post '/contact', to: 'contact#create'
+  post '/identification', to: 'identification#create'
   mount LetsEncrypt::Engine => '/.well-known'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
